85cd850be167b21c59db8d8534334f6dee347ac2
wanggw <350113733@qq.com>
2017-05-09 16:35:59 +0800

    修改了Mapper

---
+++ src/main/java/cn/dceast/dao/ProgrammerMapper.java
@@ -0,0 +1,7 @@
+package cn.dceast.dao;
+
+import cn.dceast.model.Programmer;
+import tk.mybatis.mapper.common.Mapper;
+@org.apache.ibatis.annotations.Mapper
+public interface ProgrammerMapper extends Mapper<Programmer> {
+}
